978c378de440824cd676ce1c0dfe4d2ba21bdd27
MasoudMac <shofahi@hotmail.com>
2016-11-24 14:57:04 +0100

    New Files

--- src/NormalDefender.java
+++ src/NormalDefender.java
@@ -11,7 +11,6 @@ public class NormalDefender extends Defender {
 
     private Queue <Attacker> enemyList = new LinkedList<>();
 
-    private boolean enemyDetected = false;
 
     public NormalDefender(Position pos, LinkedList<Attacker> attackersList) {
         super(pos, DAMAGE, RANGE, FIRE_RATE,attackersList);
@@ -33,22 +32,34 @@ public class NormalDefender extends Defender {
                 enemyList.remove();
             }
 
-            if(!enemyList.isEmpty()){
-                enemyList.peek().inflictDamage(DAMAGE);
+            if(!enemyList.isEmpty() &&enemyList.peek().getHealth() == 0){
+                System.out.println("Removing from the queue");
+                enemyList.peek().inflictDamage(1);
+                enemyList.remove();
             }
 
+            if(!enemyList.isEmpty()){
+                enemyList.peek().inflictDamage(DAMAGE);
+                break;
+            }
         }
     }
 
     @Override
     void render(Graphics g) {
         g.drawImage(towerImg,getPos().getX(),getPos().getY(),towerImg.getWidth(),towerImg.getHeight(),null);
+
         g.setColor(Color.blue);
         g.drawRect(getPos().getX()-(getRange()/2)+(towerImg.getWidth()/2),getPos().getY()-(getRange()/2)+(towerImg.getHeight()/2),getRange(),getRange());
 
         if(!enemyList.isEmpty()){
             g.drawLine(getPos().getX(),getPos().getY(),enemyList.peek().getPos().getX()+10,enemyList.peek().getPos().getY()+10);
         }
+
+        //System.out.println("Size of Queue is: " + enemyList.size());
+
+
+
     }
 
     @Override
