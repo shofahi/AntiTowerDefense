275f7b4bcc0498b70ae9d1a42f01fd398c005a57
MasoudMac <shofahi@hotmail.com>
2016-11-24 14:23:06 +0100

    New Files

--- src/NormalAttacker.java
+++ src/NormalAttacker.java
@@ -1,4 +1,4 @@
-import javafx.geometry.Pos;
+import com.sun.corba.se.impl.orbutil.graph.Graph;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -10,9 +10,10 @@ import java.util.LinkedList;
  */
 public class NormalAttacker extends Attacker {
     static private final int SPEED = 10;            //Default speed for Normal
-    static private final int START_HEALTH = 100;    //Default health for Normal
+    static private final int START_HEALTH = 1000;    //Default health for Normal
     static private final int ATTACKER_WIDTH = 20;
     static private final int ATTACKER_HEIGHT = 20;
+
 
     private BufferedImage normatAttackerImg;
 
@@ -20,10 +21,16 @@ public class NormalAttacker extends Attacker {
     public NormalAttacker(Position pos, LinkedList<Block> directionSign){
         super(pos, directionSign, START_HEALTH, SPEED,ATTACKER_WIDTH,ATTACKER_HEIGHT);
         normatAttackerImg = loadImage.loadTheImage("Monster1.png");
+
     }
 
     public void render(Graphics g){
         g.drawImage(normatAttackerImg,getPos().getX(),getPos().getY(),getWidth(),getHeight(),null);
+
+        g.setColor(Color.RED);
+        g.drawRect(getPos().getX(),getPos().getY()-10,getHealthBar().width-1,getHealthBar().height-1);
+        g.setColor(Color.GREEN);
+        g.fillRect(getPos().getX(),getPos().getY()-getWidth()/2,getHealthBar().width*(getHealth()/START_HEALTH),getHealthBar().height);
     }
 
     public void getTurn() {
@@ -67,19 +74,14 @@ public class NormalAttacker extends Attacker {
     }
 
     public void inflictDamage(int dmg) {
-        int currentHP = this.getHealth();
-        int newHP = currentHP - dmg;
 
-        if (newHP <= 0) {
-            //TODO: KILL the attacker somehow
-            System.out.println("DEAD");
-        } else {
-            this.setHealth(newHP);
-        }
+        setHealth(getHealth()-dmg);
+
     }
 
     @Override
     public Rectangle getBound() {
+
         return new Rectangle(getPos().getX(),getPos().getY(),getWidth(),getHeight());
     }
 }
--- src/NormalDefender.java
+++ src/NormalDefender.java
@@ -1,16 +1,17 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.LinkedList;
+import java.util.*;
 
-/**
- * Created by MasoudMac on 24/11/16.
- */
 public class NormalDefender extends Defender {
-    static private final int DAMAGE = 10;
+    static private final int DAMAGE = 1;
     static private final int RANGE = 300;
     static private final int FIRE_RATE = 3;
     private BufferedImage towerImg;
     private LoadImage loadImage = new LoadImage();
+
+    private Queue <Attacker> enemyList = new LinkedList<>();
+
+    private boolean enemyDetected = false;
 
     public NormalDefender(Position pos, LinkedList<Attacker> attackersList) {
         super(pos, DAMAGE, RANGE, FIRE_RATE,attackersList);
@@ -21,10 +22,21 @@ public class NormalDefender extends Defender {
     @Override
     void update() {
 
+
         for(int i = 0; i< getAttackersList().size(); i++){
-            if(getBound().intersects(getAttackersList().get(i).getBound())){
-                System.out.println("Pang pang");
+
+            if(getRageBound().intersects(getAttackersList().get(i).getBound()) && !enemyList.contains(getAttackersList().get(i))) {
+                enemyList.add(getAttackersList().get(i));
             }
+
+            if(!enemyList.isEmpty() && !enemyList.peek().getBound().intersects(getRageBound())) {
+                enemyList.remove();
+            }
+
+            if(!enemyList.isEmpty()){
+                enemyList.peek().inflictDamage(DAMAGE);
+            }
+
         }
     }
 
@@ -33,6 +45,10 @@ public class NormalDefender extends Defender {
         g.drawImage(towerImg,getPos().getX(),getPos().getY(),towerImg.getWidth(),towerImg.getHeight(),null);
         g.setColor(Color.blue);
         g.drawRect(getPos().getX()-(getRange()/2)+(towerImg.getWidth()/2),getPos().getY()-(getRange()/2)+(towerImg.getHeight()/2),getRange(),getRange());
+
+        if(!enemyList.isEmpty()){
+            g.drawLine(getPos().getX(),getPos().getY(),enemyList.peek().getPos().getX()+10,enemyList.peek().getPos().getY()+10);
+        }
     }
 
     @Override
@@ -42,6 +58,7 @@ public class NormalDefender extends Defender {
 
     @Override
     public Rectangle getRageBound() {
-        return new Rectangle(getPos().getX(),getPos().getY(),getRange(),getRange());
+        return new Rectangle(getPos().getX()-(getRange()/2)+(towerImg.getWidth()/2),getPos().getY()-(getRange()/2)+
+                    (towerImg.getHeight()/2),getRange(),getRange());
     }
 }
