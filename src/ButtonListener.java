114031b96155d63673e8f132492a7c78a616d8b8
Samuel <dv11sfd@cs.umu.se>
2016-12-18 10:13:24 +0100

    worldhandler kommentarer

--- src/ButtonListener.java
+++ src/ButtonListener.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
@@ -8,16 +7,27 @@ public class ButtonListener implements ActionListener{
 
     private LinkedList<ActionEvent> listOfActions;
 
+    /**
+     * Constructor which creates LinkedList filled with
+     * ActionEvents
+     */
     public ButtonListener(){
         listOfActions = new LinkedList<ActionEvent>();
     }
 
+    /**
+     * Adds action event to list of actions
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
 
         listOfActions.add(e);
     }
 
+    /**
+     * Returns the list of ActionsEvents
+     * @return LinkedList<ActionEvent> listOfActions
+     */
     public LinkedList<ActionEvent> getListOfActions() {
         return listOfActions;
     }
