f64c2383c786fe0cb47dc2d8de86713409e877fa
Samuel <dv11sfd@cs.umu.se>
2016-12-19 12:51:22 +0100

    Kommentarer och kodkonvention klar. Lägg till/Ta bort namn i metod/klasskommentar för att handledare ska kunna veta vem som gjort vad

--- src/Position.java
+++ src/Position.java
@@ -19,6 +19,7 @@ public class Position {
      * @param y The y value of the position.
      */
     public Position(int x, int y) {
+
         this.x = x;
         this.y = y;
     }
@@ -56,12 +57,15 @@ public class Position {
      */
     @Override
     public boolean equals(Object o) {
+
         if (this == o) return true;
+
         if (o == null || getClass() != o.getClass()) return false;
 
         Position position = (Position) o;
 
         if (x != position.x) return false;
+
         return y == position.y;
     }
 
@@ -71,8 +75,10 @@ public class Position {
      */
     @Override
     public int hashCode() {
+
         int result = x;
         result = 31 * result + y;
+
         return result;
     }
 }
